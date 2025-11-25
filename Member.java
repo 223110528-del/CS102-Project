fad667c89313e233f0f82af84b2faf80f3fdf8d2
MOHAMMED FAHAD  MOHAMMED  ALSALEM <223110528@psu.edu.sa>
2025-11-25 11:08:44 +0300

    Creates the member id and member name for borrowing books

---
+++ Member.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.library;
+
+/**
+ *
+ * @author 223110528
+ */
+public class Member {
+    private String name;
+    private String memberId;
+
+    public Member(String name, String memberId) {
+        this.name = name;
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Member ID: " + memberId;
+    }
+}    
+
