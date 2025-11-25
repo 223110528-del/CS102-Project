25e65644f192bf9600ad3c6b9a7dbdc8c149dee7
MOHAMMED FAHAD  MOHAMMED  ALSALEM <223110528@psu.edu.sa>
2025-11-25 11:07:21 +0300

    Creates book name and author title and book publish date

---
+++ Book.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.library;
+
+/**
+ *
+ * @author 223110528
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year > 0) { this.year = year;
+        } else { System.out.println("Invalid year.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Year: " + year;
+    }    
+}
